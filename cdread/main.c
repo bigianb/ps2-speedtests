870a4a380a4f7ce1bf79a4ccf8af0ccebcc13769
Ian Brown <bigianb@gmail.com>
2021-01-22 00:01:49 +0000

    more file reading

--- cdread/main.c
+++ cdread/main.c
@@ -1,15 +1,16 @@
 #include <tamtypes.h>
 #include <stdio.h>
+#include <libcdvd.h>
+#include <sifrpc.h>
 
 int data[255];
 
 // Write 255 million words in a tight loop.
 void writeWordsInLoop()
 {
-	int i, j;
 	// Lots of writes to normal memory.
-	for (j=0; j<100; ++j){
-		for(i=0; i<255; ++i){
+	for (int j=0; j<100; ++j){
+		for(int i=0; i<255; ++i){
 			data[i] = i;
 		}
 	}
@@ -21,18 +22,37 @@ void writeWordsInLoop()
 
 void timeFunc(const char* name, void (*fun)())
 {
-	u32 startTime, endTime, clocksPerSec;
 	fun(); // warm-up
 	printf("%s\n", name);
-	startTime = *((u32*)CLOCK_ADDR);
+	u32 startTime = *((u32*)CLOCK_ADDR);
 	fun();
-	endTime = *((u32*)CLOCK_ADDR);
-	clocksPerSec = *((u32*)CLOCKS_PER_SEC_ADDR);
+	u32 endTime = *((u32*)CLOCK_ADDR);
+	u32 clocksPerSec = *((u32*)CLOCKS_PER_SEC_ADDR);
 	printf("elapsed clocks=%d, clocksPerSec=%d\n", endTime-startTime, clocksPerSec);
 }
 
 int main()
 {
+	SifInitRpc(0);
+	sceCdInit(SCECdINIT);
+	
+	sceCdlFILE file;
+	int ret = sceCdSearchFile(&file, "\\ZERO5M.BIN");
+
+	printf("search name %s\n", file.name);
+	printf("search size %d\n", file.size);
+	printf("search loc lnn %d\n", file.lsn);
+	printf("search loc date %02X %02X %02X %02X %02X %02X %02X %02X\n",
+			file.date[0], file.date[1], file.date[2], file.date[3],
+			file.date[4], file.date[5], file.date[6], file.date[7]);
+	printf("search loc date %02d %02d %02d %02d %02d %02d %02d %02d\n",
+			file.date[0], file.date[1], file.date[2], file.date[3],
+			file.date[4], file.date[5], file.date[6], file.date[7]);
+
+
+	//ret = sceCdRead(fp.lsn, sectors, buf, &mode);
+	//sceCdSync(0);
+
 	timeFunc("Write Words in loop", writeWordsInLoop);
 	return 0;
 }
