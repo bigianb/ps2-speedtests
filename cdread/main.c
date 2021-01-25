196ddc6b5b928a29be5fb1db483b09995b8a656f
Ian Brown <bigianb@gmail.com>
2021-01-25 18:48:27 +0000

    file reading

--- cdread/main.c
+++ cdread/main.c
@@ -1,20 +1,8 @@
 #include <tamtypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <libcdvd.h>
 #include <sifrpc.h>
-
-int data[255];
-
-// Write 255 million words in a tight loop.
-void writeWordsInLoop()
-{
-	// Lots of writes to normal memory.
-	for (int j=0; j<100; ++j){
-		for(int i=0; i<255; ++i){
-			data[i] = i;
-		}
-	}
-}
 
 #define CLOCK_ADDR 0x10001900
 #define CLOCKS_PER_SEC_ADDR 0x10001904
@@ -22,13 +10,46 @@ void writeWordsInLoop()
 
 void timeFunc(const char* name, void (*fun)())
 {
-	fun(); // warm-up
 	printf("%s\n", name);
 	u32 startTime = *((u32*)CLOCK_ADDR);
 	fun();
 	u32 endTime = *((u32*)CLOCK_ADDR);
 	u32 clocksPerSec = *((u32*)CLOCKS_PER_SEC_ADDR);
-	printf("elapsed clocks=%d, clocksPerSec=%d\n", endTime-startTime, clocksPerSec);
+	printf("\nelapsed clocks=%d, clocksPerSec=%d\n", endTime-startTime, clocksPerSec);
+}
+
+// Read a 5Mb file 100 times.
+void readFile()
+{
+	sceCdlFILE file;
+	int ret = sceCdSearchFile(&file, "\\ZERO5M.BIN;1");
+	printf("search file return value: %d\n", ret);
+	printf("name %s\n", file.name);
+	printf("size %d\n", file.size);
+	printf("loc %d\n", file.lsn);
+	printf("date %02X %02X %02X %02X %02X %02X %02X %02X\n",
+			file.date[0], file.date[1], file.date[2], file.date[3],
+			file.date[4], file.date[5], file.date[6], file.date[7]);
+
+	const int SECTOR_SIZE = 2048;
+	const int sectorsToRead = (file.size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+
+	sceCdRMode mode;
+	mode.trycount = 32;
+	mode.spindlctrl = SCECdSpinNom;
+	mode.datapattern = SCECdSecS2048;
+	mode.pad = 0;
+
+	printf("Sectors to read: %d\n", sectorsToRead);
+
+	const int bufSize = sectorsToRead * SECTOR_SIZE;
+	char* buf = malloc(bufSize);
+
+	for (int i=0; i<20; ++i){
+		sceCdRead(file.lsn, sectorsToRead, buf, &mode);
+		sceCdSync(0);
+	}
+	free(buf);
 }
 
 int main()
@@ -36,23 +57,7 @@ int main()
 	SifInitRpc(0);
 	sceCdInit(SCECdINIT);
 	
-	sceCdlFILE file;
-	int ret = sceCdSearchFile(&file, "\\ZERO5M.BIN");
-
-	printf("search name %s\n", file.name);
-	printf("search size %d\n", file.size);
-	printf("search loc lnn %d\n", file.lsn);
-	printf("search loc date %02X %02X %02X %02X %02X %02X %02X %02X\n",
-			file.date[0], file.date[1], file.date[2], file.date[3],
-			file.date[4], file.date[5], file.date[6], file.date[7]);
-	printf("search loc date %02d %02d %02d %02d %02d %02d %02d %02d\n",
-			file.date[0], file.date[1], file.date[2], file.date[3],
-			file.date[4], file.date[5], file.date[6], file.date[7]);
-
-
-	//ret = sceCdRead(fp.lsn, sectors, buf, &mode);
-	//sceCdSync(0);
-
-	timeFunc("Write Words in loop", writeWordsInLoop);
+	timeFunc("read file", readFile);
+	
 	return 0;
 }
--- results.md
+++ results.md
@@ -5,6 +5,7 @@
 
 | Build | clocks |
 | ----- | ------ |
+| PCSX2-32 16-1-21 | 17360  |
 | Play master 15-1-21 | 13001 |
 | Play fastmem        | 12992 |
 
@@ -12,8 +13,17 @@
 
 | Build | clocks |
 | ----- | ------ |
+| PCSX2-32 16-1-21 | 12615  |
 | Play master 15-1-21  |  8805 |
 | Play fastmem         |  8792 |
+
+## Read 5Mb file 100 times
+
+| Build | clocks |
+| ----- | ------ |
+| PCSX2-32 16-1-21 | 77025  |
+| Play master 15-1-21 | 774 |
+
 
 # Dell XPS 15 laptop, i7-4702HQ CPU @ 2.20GHz
 
